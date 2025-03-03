ec7cdb42e06ae5fa07c0187f0dde6ef476a4eed3
Rae <rae.dambach@gmail.com>
2025-03-03 22:24:25 +0100

    added Jupyter Notebook

--- src/data/fetch_data.py
+++ src/data/fetch_data.py
@@ -1,11 +1,25 @@
 import requests
+import ee
+import geemap  
+import folium  
+
+ee.Authenticate()
+ee.Initialize(project='ee-raedambach')
+longitude = -149.56194 
+latitude = -17.00872
 
 
 def download_satellite_data():
     # Placeholder for downloading satellite data
     print('Downloading satellite images...')
+    reef = ee.Geometry.Point([longitude, latitude])  
+    dataset = ee.ImageCollection('COPERNICUS/S2_HARMONIZED').filterBounds(reef).filterDate('2024-01-01', '2024-01-02').median()  
+    Map = geemap.Map()  
+    Map.centerObject(reef, 10)  
+    Map.addLayer(dataset, {'bands': ['B4', 'B3', 'B2'], 'min': 0, 'max': 3000}, 'Sentinel-2')  
+    Map 
 
-
+    print('Finished')
 
 if __name__ == '__main__':
     download_satellite_data()
